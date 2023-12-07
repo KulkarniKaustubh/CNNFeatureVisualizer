3c56f15633861065388fc0502174932363ffc65b
KulkarniKaustubh <kaustubhmilind.kulkarni@colorado.edu>
2023-12-07 11:33:42 -0700

    create worker folder and restructure

--- src/worker.py
+++ src/worker.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 import subprocess
 import redis
 from minio import Minio
@@ -7,7 +8,39 @@ import os
 import glob
 import io
 
+import torch
+import torch.nn as nn
+
 from torchboard.utils import load_model_weights
+
+
+def _get_model(source_code, layer_weights_file):
+    spec = importlib.util.spec_from_file_location(
+        "received_model_module", source_code
+    )
+    received_model_module = importlib.util.module_from_spec(spec)
+    received_model_module.__dict__["torch"] = torch
+    received_model_module.__dict__["nn"] = nn
+    spec.loader.exec_module(received_model_module)
+
+    # Get the actual class name dynamically
+    model_class_name = [
+        name
+        for name, obj in received_model_module.__dict__.items()
+        if isinstance(obj, type)
+    ][0]
+
+    # Use the actual class name to instantiate the model
+    ReceivedModel = getattr(received_model_module, model_class_name)
+
+    # Create an instance of the received model class
+    received_model_instance = ReceivedModel()
+
+    received_model_instance = load_model_weights(
+        received_model_instance, layer_weights_file
+    )
+
+    return received_model_instance
 
 
 def init_redis() -> redis.StrictRedis:
@@ -20,12 +53,15 @@ def init_redis() -> redis.StrictRedis:
 
 
 def init_minio() -> Minio:
-    minio_host = os.getenv("MINIO_HOST") or "minio:9000"
+    minio_host = os.getenv("MINIO_HOST") or "localhost"
+    minio_port = os.getenv("MINIO_PORT") or 9000
     minio_user = os.getenv("MINIO_USER") or "rootuser"
     minio_passwd = os.getenv("MINIO_PASSWD") or "rootpass123"
 
+    minio_final_addr = f"{minio_host}:{minio_port}"
+
     minio_client = Minio(
-        minio_host,
+        minio_final_addr,
         secure=False,
         access_key=minio_user,
         secret_key=minio_passwd,
@@ -52,17 +88,59 @@ def check_minio_objects(minio_client, bucket_name):
         print("Minio objects are empty or do not exist.")
 
 
-def read_queue() -> nn.Module:
-    model = None
-    weights_file = None
+def get_model_from_queue(
+    redis_client: redis.StrictRedis, minio_client: Minio
+) -> nn.Module:
+    redis_queue = "toWorkers"
 
-    return model, weights_file
+    bucket_name_source_code = "source-code"
+    bucket_name_layer_weights = "layer-weights"
+    source_code_file_location = "received_model_source_code.py"
+    layer_weights_file_path = "layer_weights.pth"
+
+    work = redis_client.blpop(redis_queue, timeout=0)
+
+    model_name = work[1].decode("utf-8").split(".")[0].split(":")[1].strip()
+    layer_weights_minio_path = (
+        work[1].decode("utf-8").split(".")[1].split(":")[1].strip()
+    )
+
+    model = None
+    response = None
+
+    try:
+        print("Model Name : ", model_name)
+        print("Layer_Weights Minio Path: ", layer_weights_minio_path)
+        response = minio_client.fget_object(
+            bucket_name_source_code, model_name, source_code_file_location
+        )
+        print("Recieved source code in location: ", source_code_file_location)
+        response = minio_client.fget_object(
+            bucket_name_layer_weights,
+            layer_weights_minio_path,
+            layer_weights_file_path,
+        )
+        print("Recieved Layer Weights in location: ", layer_weights_file_path)
+
+        model = _get_model(
+            source_code=source_code_file_location,
+            layer_weights_file=layer_weights_file_path,
+        )
+    finally:
+        if response is not None:
+            response.close()
+            response.release_conn()
+
+    return model
 
 
 def main():
+    redis_client = init_redis()
+    minio_client = init_minio()
+    print(minio_client)
+
     while True:
-        model, weights_file = read_queue()
-        model = load_model_weights(model, weights_file)
+        model = get_model_from_queue(redis_client, minio_client)
 
         cnn_layer_viz = tb.visualizers.CNNLayerVisualization(model, 0, 5)
 
@@ -74,21 +152,22 @@ def main():
 
 
 if __name__ == "__main__":
-    redis_client = init_redis()
-    minio_client = init_minio()
-    print(minio_client)
+    try:
+        main()
+    except Exception as exp:
+        print(f"Exception raised in main(): {str(exp)}")
 
-    bucket_name = "queue"
-    redis_queue = "toWorker"
-    output_bucket_name = "output"
-
-    if not minio_client.bucket_exists(bucket_name):
-        print(f"Create bucket {bucket_name}")
-        minio_client.make_bucket(bucket_name)
-
-    if not minio_client.bucket_exists(output_bucket_name):
-        print(f"Create bucket {output_bucket_name}")
-        minio_client.make_bucket(output_bucket_name)
+    # bucket_name = "queue"
+    # redis_queue = "toWorker"
+    # output_bucket_name = "output"
+    #
+    # if not minio_client.bucket_exists(bucket_name):
+    #     print(f"Create bucket {bucket_name}")
+    #     minio_client.make_bucket(bucket_name)
+    #
+    # if not minio_client.bucket_exists(output_bucket_name):
+    #     print(f"Create bucket {output_bucket_name}")
+    #     minio_client.make_bucket(output_bucket_name)
 
     # while True:
     #     print("In while")
@@ -143,6 +222,6 @@ if __name__ == "__main__":
     #         traceback.print_exc()
     #         print(f"Exception raised in log loop: {str(exp)}")
 
-    while True:
-        print("Hello from the worker!")
-        time.sleep(2)
+    # while True:
+    #     print("Hello from the worker!")
+    #     time.sleep(2)
---
+++ worker/old_worker.py
@@ -0,0 +1,112 @@
+import sys
+import os
+import redis
+from minio import Minio
+import torch
+import torch.nn as nn
+import importlib
+
+redisHost = os.getenv("REDIS_HOST") or "localhost"
+redisPort = os.getenv("REDIS_PORT") or 6379
+minioHost = os.getenv("MINIO_HOST") or "localhost"
+minioPort = os.getenv("MINIO_PORT") or 9000
+redis_client = redis.StrictRedis(host=redisHost, port=redisPort, db=0)
+minioUser = "rootuser"
+minioPasswd = "rootpass123"
+minioFinalAddress = minioHost + ":" + minioPort
+minio_client = Minio(
+    minioFinalAddress,
+    secure=False,
+    access_key=minioUser,
+    secret_key=minioPasswd,
+)
+
+
+def get_model_output(model):
+    example_input = torch.randn(1, 3, 255, 255)
+    output = model(example_input)
+    return output
+
+
+def get_model(source_code, layer_weights_file):
+    spec = importlib.util.spec_from_file_location(
+        "received_model_module", source_code
+    )
+    received_model_module = importlib.util.module_from_spec(spec)
+    received_model_module.__dict__["torch"] = torch
+    received_model_module.__dict__["nn"] = nn
+    spec.loader.exec_module(received_model_module)
+
+    # Get the actual class name dynamically
+    model_class_name = [
+        name
+        for name, obj in received_model_module.__dict__.items()
+        if isinstance(obj, type)
+    ][0]
+
+    # Use the actual class name to instantiate the model
+    ReceivedModel = getattr(received_model_module, model_class_name)
+
+    # Create an instance of the received model class
+    received_model_instance = ReceivedModel()
+    layer_weights = torch.load(layer_weights_file)
+
+    print("received model is: ", received_model_instance)
+
+    # Load each layer's weights back into the model
+    for name, param in received_model_instance.named_parameters():
+        param.data.copy_(layer_weights[name])
+    # received_model_instance.to("mps")
+    return received_model_instance
+
+
+bucket_name_source_code = "source-code"
+bucket_name_layer_weights = "layer-weights"
+source_code_file_location = "received_model_source_code.py"
+layer_weights_file_path = "layer_weights.pth"
+
+while True:
+    try:
+        work = redis_client.blpop("toWorkers", timeout=0)
+        model_name = (
+            work[1].decode("utf-8").split(".")[0].split(":")[1].strip()
+        )
+        layer_weights_minio_path = (
+            work[1].decode("utf-8").split(".")[1].split(":")[1].strip()
+        )
+        response = None
+        # Get data of an object.
+        try:
+            print("Model Name : ", model_name)
+            print("Layer_Weights Minio Path: ", layer_weights_minio_path)
+            response = minio_client.fget_object(
+                bucket_name_source_code, model_name, source_code_file_location
+            )
+            print(
+                "Recieved source code in location: ", source_code_file_location
+            )
+            response = minio_client.fget_object(
+                bucket_name_layer_weights,
+                layer_weights_minio_path,
+                layer_weights_file_path,
+            )
+            print(
+                "Recieved Layer Weights in location: ", layer_weights_file_path
+            )
+            model = get_model(
+                source_code=source_code_file_location,
+                layer_weights_file=layer_weights_file_path,
+            )
+            print("Model has been Loaded Succesfully")
+            output = get_model_output(model=model)
+            print("Output is: ", output)
+        # Read data from response.
+        finally:
+            if response is not None:
+                response.close()
+                response.release_conn()
+    except Exception as exp:
+        print(f"Exception raised in log loop: {str(exp)}")
+
+    sys.stdout.flush()
+    sys.stderr.flush()
