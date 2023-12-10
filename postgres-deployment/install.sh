74fd12d7cc75f9168d7f708fad72f4834dc97111
Jayant-Duneja <jayant.duneja@colorado.edu>
2023-12-10 16:22:38 -0700

    operations on Postgres

---
+++ postgres-deployment/install.sh
@@ -0,0 +1,4 @@
+kubectl apply -f postgres-config.yaml
+kubectl apply -f postgres-pvc-pv.yaml
+kubectl apply -f postgres-deployment.yaml
+kubectl apply -f postgres-service.yaml
--- rest-server/rest-application/app/routes.py
+++ rest-server/rest-application/app/routes.py
@@ -15,6 +15,9 @@ from flask import (
     send_file,
 )
 from flask import g
+from flask import request, jsonify
+
+
 
 # import io
 import hashlib
@@ -24,8 +27,8 @@ import random
 # import base64
 import torch
 
-# import psycopg2
-# from psycopg2 import sql
+import psycopg2
+from psycopg2 import sql
 import importlib
 import torch.nn as nn  # Include the necessary import
 import tempfile
@@ -37,7 +40,12 @@ conn = None
 redisHost = os.getenv("REDIS_HOST") or "localhost"
 redisPort = os.getenv("REDIS_PORT") or 6379
 minioHost = os.getenv("MINIO_HOST") or "localhost"
-minioPort = os.getenv("MINIO_PORT") or 9000
+minioPort = os.getenv("MINIO_PORT") or "9000"
+postgresHost = os.getenv("POSTGRES_HOST") or "localhost"
+postgresPort = os.getenv("POSTGRES_PORT") or 5432
+postgresUser = 'admin'
+postgresPassword = 'psltest'
+postgresDbname = 'postgresdb'
 redisClient = redis.StrictRedis(host=redisHost, port=redisPort, db=0)
 minioUser = "rootuser"
 minioPasswd = "rootpass123"
@@ -48,9 +56,6 @@ minioClient = Minio(
     access_key=minioUser,
     secret_key=minioPasswd,
 )
-# bucketName = "queue"
-
-
 def get_model_output(model):
     model.eval()
     example_input = torch.randn(1, 3, 255, 255)
@@ -63,46 +68,33 @@ def get_model_output(model):
 def hello():
     return "Hi. Welcome to the rest-server"
 
-
-@app.route("/postgres/connect", methods=["GET"])
-def testPostgresConnection():
+# Move this to the Init of the Rest-Server. We should always connect to Postgres first before doing all the operations
+# Function to establish a PostgreSQL connection
+def connect_to_postgres():
     global conn
     try:
-        # Connection object for the case it is deployed on the container
-        # conn = psycopg2.connect(
-        #         host='postgres',
-        #         port=5432,
-        #         user='admin',
-        #         password='psltest',
-        #         dbname='postgresdb'
-        # )
         conn = psycopg2.connect(
-            host="localhost",
-            port=5432,
-            user="admin",
-            password="psltest",
-            dbname="postgresdb",
+            host=postgresHost,
+            port=postgresPort,
+            user=postgresUser,
+            password=postgresPassword,
+            dbname=postgresDbname,
         )
-        return "Fuck yeah!"
+        print("PostgreSQL connection established.")
     except Exception as e:
-        return "Exception: " + e
+        print("Exception: " + str(e))
+
+# Execute the connect_to_postgres function before the first request
+@app.before_request
+def before_request():
+    if not hasattr(app, 'postgres_connected'):
+        connect_to_postgres()
+        app.postgres_connected = True
 
 
-@app.route("/postgres/createTable", methods=["POST"])
-def create_table():
+def create_table_helper(table_name, columns):
     try:
         with conn.cursor() as cursor:
-            # Define the table name
-            table_name = "modeloutput"
-
-            # Define the column names and data types
-            columns = [
-                ("username", "VARCHAR PRIMARY KEY"),
-                ("modelname", "VARCHAR"),
-                ("iterationnumber", "INT"),
-                ("output", "FLOAT"),
-            ]
-
             # Generate the CREATE TABLE statement
             create_table_query = sql.SQL("CREATE TABLE {} ({});").format(
                 sql.Identifier(table_name),
@@ -117,57 +109,216 @@ def create_table():
             # Execute the CREATE TABLE statement
             cursor.execute(create_table_query)
             conn.commit()
-        cursor.close()
+
         response_data = {"response": "Success. Table has been created"}
         return jsonify(response_data)
+
     except Exception as e:
-        response_data = {"response": "Failed", "Exception": e}
+        conn.rollback()
+        response_data = {"response": "Failed", "Exception": str(e)}
+        return jsonify(response_data)
+
+@app.route("/postgres/createTable", methods=["POST"])
+def create_custom_table():
+    try:
+        # Get data from the request body
+        request_data = request.get_json()
+        table_name = request_data.get("table_name")
+        columns = request_data.get("columns")
+
+        # Check if required data is present
+        if not table_name or not columns:
+            raise ValueError("Table name and columns are required in the request body")
+
+        # Call the create_table function with the provided parameters
+        response = create_table_helper(table_name, columns)
+        return response
+
+    except Exception as e:
+        response_data = {"response": "Failed", "Exception": str(e)}
+        return jsonify(response_data)
+    
+@app.route("/postgres/getSchema", methods=["POST"])
+def get_schema():
+    try:
+        # Get data from the request body
+        request_data = request.get_json()
+        table_name = request_data.get("table_name")
+
+        # Check if required data is present
+        if not table_name:
+            raise ValueError("Table name is required in the request body")
+
+        with conn.cursor() as cursor:
+            query = """
+            SELECT column_name, data_type
+            FROM information_schema.columns
+            WHERE table_schema = 'public'  -- You can adjust this condition based on your schema
+            AND table_name = %s;
+            """
+
+            # Execute the query with the table name parameter
+            cursor.execute(query, (table_name,))
+
+            # Fetch all the results
+            columns_info = cursor.fetchall()
+
+            # Display the result
+            for column_info in columns_info:
+                print(f"Column Name: {column_info[0]}, Data Type: {column_info[1]}")
+
+        cursor.close()
+        response_data = {"response": "Success"}
+        return jsonify(response_data)
+
+    except Exception as e:
+        conn.rollback()
+        response_data = {"response": "Failed", "Exception": str(e)}
+        return jsonify(response_data)
+@app.route("/postgres/deleteTable", methods=["POST"])
+def delete_table():
+    try:
+        # Get data from the request body
+        request_data = request.get_json()
+        table_name = request_data.get("table_name")
+
+        # Check if required data is present
+        if not table_name:
+            raise ValueError("Table name is required in the request body")
+
+        with conn.cursor() as cursor:
+            # Generate the DROP TABLE statement
+            drop_table_query = sql.SQL("DROP TABLE IF EXISTS {} CASCADE;").format(
+                sql.Identifier(table_name)
+            )
+
+            # Execute the DROP TABLE statement
+            cursor.execute(drop_table_query)
+            conn.commit()
+
+        cursor.close()
+        response_data = {"response": f"Success. Table '{table_name}' has been deleted"}
+        return jsonify(response_data)
+
+    except Exception as e:
+        conn.rollback()
+        response_data = {"response": "Failed", "Exception": str(e)}
         return jsonify(response_data)
 
 
-@app.route("/postgres/getTable", methods=["GET"])
-def get_table():
-    with conn.cursor() as cursor:
-        query = """
-    SELECT table_name
-    FROM information_schema.tables
-    WHERE table_schema = 'public'  -- You can adjust this condition based on your schema
-    AND table_type = 'BASE TABLE';
-    """
+@app.route("/postgres/getRows", methods=["POST"])
+def get_rows():
+    try:
+        # Get data from the request body
+        request_data = request.get_json()
+        table_name = request_data.get("table_name")
 
-        # Execute the query
-        cursor.execute(query)
+        # Check if required data is present
+        if not table_name:
+            raise ValueError("Table name is required in the request body")
 
-        # Fetch all the results
-        table_names = cursor.fetchall()
+        with conn.cursor() as cursor:
+            # Generate the SELECT query with the provided table name
+            select_query = f"SELECT * FROM {table_name};"
+
+            # Execute the SELECT query
+            cursor.execute(select_query)
+
+            # Fetch all rows
+            rows = cursor.fetchall()
+            for row in rows:
+                print(row)
 
         # Display the result
-        for table_name in table_names:
-            print(table_name[0])
-    cursor.close()
-    response_data = {"response": "Success"}
-    return jsonify(response_data)
+        cursor.close()
+        response_data = {"response": "Success", "rows": rows}
+        return jsonify(response_data)
+
+    except Exception as e:
+        conn.rollback()
+        response_data = {"response": "Failed", "Exception": str(e)}
+        return jsonify(response_data)
 
 
-@app.route("/postgres/getRows", methods=["GET"])
-def get_rows():
-    with conn.cursor() as cursor:
-        table_name = "modeloutput"
+@app.route("/postgres/insertRows", methods=["POST"])
+def insert_rows():
+    try:
+        # Get data from the request body
+        request_data = request.get_json()
+        table_name = request_data.get("table_name")
+        rows_to_insert = request_data.get("rows")
 
-        # Generate the SELECT query
-        select_query = f"SELECT * FROM {table_name};"
+        # Check if required data is present
+        if not table_name or not rows_to_insert:
+            raise ValueError("Table name and rows are required in the request body")
 
-        # Execute the SELECT query
-        cursor.execute(select_query)
+        with conn.cursor() as cursor:
+            # Generate the INSERT query with the provided table name and rows
+            insert_query = sql.SQL("INSERT INTO {} VALUES {};").format(
+                sql.Identifier(table_name),
+                sql.SQL(", ").join(
+                    sql.SQL("({})").format(
+                        sql.SQL(", ").join(sql.Literal(value) for value in row)
+                    )
+                    for row in rows_to_insert
+                ),
+            )
 
-        # Fetch all rows
-        rows = cursor.fetchall()
-        for row in rows:
-            print(row)
-    # Display the result
-    cursor.close()
-    response_data = {"response": "Success", "rows": rows}
-    return jsonify(response_data)
+            # Execute the INSERT query
+            cursor.execute(insert_query)
+            conn.commit()
+
+        cursor.close()
+        response_data = {"response": "Success. Rows have been inserted"}
+        return jsonify(response_data)
+
+    except Exception as e:
+        conn.rollback()
+        response_data = {"response": "Failed", "Exception": str(e)}
+        return jsonify(response_data)
+
+@app.route("/postgres/getLosses", methods=["POST"])
+def get_losses_epochs():
+    try:
+        # Get data from the request body
+        request_data = request.get_json()
+        model_hash = request_data.get("model_hash")
+        username = request_data.get("username")
+
+        # Check if required data is present
+        if not model_hash or not username:
+            raise ValueError("Both model_hash and username are required in the request body")
+
+        with conn.cursor() as cursor:
+            # Define the query to select losses and epochs for a specific model_hash and username
+            query = sql.SQL("SELECT epoch, losses FROM training_metrics WHERE model_hash = {} AND username = {};").format(
+                sql.Literal(model_hash),
+                sql.Literal(username)
+            )
+
+            # Execute the query
+            cursor.execute(query)
+
+            # Fetch all rows
+            rows = cursor.fetchall()
+
+        cursor.close()
+        # Format the result as a list of dictionaries
+        epochs=[]
+        losses=[]
+        result = [{"epoch": row[0], "losses": row[1]} for row in rows]
+        epochs = [row[0] for row in rows]
+        losses = [row[1] for row in rows]
+        print("Epochs : ", epochs)
+        print("Losses : ", losses)
+        print("-----------------------------------------------")
+        response_data = {"response": "Success", "data": result}
+        return jsonify(response_data)
+
+    except Exception as e:
+        conn.rollback()
+        response_data = {"response": "Failed", "Exception": str(e)}
+        return jsonify(response_data)
 
 
 def make_minio_bucket(bucket_name):
